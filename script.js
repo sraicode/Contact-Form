041d2fe8c299e85c7062ebdb5b12ad56a76eb5d1
sraicode <sbrai18@gmail.com>
2026-04-17 16:47:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,13 @@
-var sheetName = 'Sheet1'
-var scriptProp = PropertiesService.getScriptProperties()
+const scriptURL = 'Google App Script URL'
 
-function intialSetup () {
-  var activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet()
-  scriptProp.setProperty('key', activeSpreadsheet.getId())
-}
+const form = document.forms['contact-form']
 
-function doPost (e) {
-  var lock = LockService.getScriptLock()
-  lock.tryLock(10000)
-
-  try {
-    var doc = SpreadsheetApp.openById(scriptProp.getProperty('key'))
-    var sheet = doc.getSheetByName(sheetName)
-
-    var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
-    var nextRow = sheet.getLastRow() + 1
-
-    var newRow = headers.map(function(header) {
-      return header === 'timestamp' ? new Date() : e.parameter[header]
-    })
-
-    sheet.getRange(nextRow, 1, 1, newRow.length).setValues([newRow])
-
-    return ContentService
-      .createTextOutput(JSON.stringify({ 'result': 'success', 'row': nextRow }))
-      .setMimeType(ContentService.MimeType.JSON)
-  }
-
-  catch (e) {
-    return ContentService
-      .createTextOutput(JSON.stringify({ 'result': 'error', 'error': e }))
-      .setMimeType(ContentService.MimeType.JSON)
-  }
-
-  finally {
-    lock.releaseLock()
-  }
-}
+form.addEventListener('submit', e => {
+  
+  e.preventDefault()
+  
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Your form has been successfully submitted. We truly appreciate your interest and will be in touch with you shortly." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => error('Error!', error.message))
+})
